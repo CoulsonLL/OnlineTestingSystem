c7a6716b1bb18fe3b17f6301212fa2a818be3ee9
David-Zhao-1997 <550361254@qq.com>
2017-12-21 20:40:19 +0800

    【后台更新】 增加了申请考试 批准考试 拒绝考试 的方法

--- src/dao/ExamLogDAO.java
+++ src/dao/ExamLogDAO.java
@@ -1,9 +1,109 @@
 package dao;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+import entity.ExamLogEntity;
+import entity.ManagerEntity;
+import util.HibernateUtil;
+
+@SuppressWarnings("ALL")
 public class ExamLogDAO
 {
-//    public boolean applyForExam(ExamLogEntity examLogEntity)
-//    {
-//
-//    }
+    /**
+     * 申请考试
+     *
+     * @param examLogEntity 考试记录实体对象
+     *                      <p>
+     *                      <p>
+     *                      需要设置的参数有
+     *                      ExamID<br/>
+     *                      StuID<br/>
+     *                      Email<br/>
+     *                      ExamStartTime(考试开始时间)</p>
+     * @return boolean
+     */
+    public boolean applyForExam(ExamLogEntity examLogEntity)
+    {
+        try
+        {
+            examLogEntity.setApprovalResult(0);
+            examLogEntity.setApplyTime(new Timestamp(new Date().getTime()));
+            Transaction tx = null;
+            List list = null;
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            session.save(examLogEntity);
+            tx.commit();
+            session.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 通过考试请求
+     *
+     * @param examLogEntity 考试实体对象
+     * @param managerEntity 管理员实体对象
+     * @return boolean
+     */
+    public boolean approveExam(ExamLogEntity examLogEntity, ManagerEntity managerEntity)
+    {
+        try
+        {
+            examLogEntity.setApprovalResult(1);
+            examLogEntity.setMgrId(managerEntity.getMgrId());
+            Transaction tx = null;
+            List list = null;
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            session.save(examLogEntity);
+            tx.commit();
+            session.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 拒绝考试请求
+     *
+     * @param examLogEntity 考试实体对象
+     * @param managerEntity 管理员实体对象
+     * @return boolean
+     */
+    public boolean rejectExam(ExamLogEntity examLogEntity, ManagerEntity managerEntity)
+    {
+        try
+        {
+            examLogEntity.setApprovalResult(-1);
+            examLogEntity.setMgrId(managerEntity.getMgrId());
+            Transaction tx = null;
+            List list = null;
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            session.save(examLogEntity);
+            tx.commit();
+            session.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
