e99894a072a0b68a79f3236b0cc9a0974ff26307
David-Zhao-1997 <550361254@qq.com>
2017-12-22 23:53:33 +0800

    【后台更新】 修复了主线程死循环导致无法部署的bug

--- src/servlet/Servlet.java
+++ src/servlet/Servlet.java
@@ -11,18 +11,20 @@ public class Servlet extends HttpServlet
     @Override
     public void init() throws ServletException
     {
-        for (; ; )
-        {
-            StudentDAO.updateStudentNum();
-            ManagerDAO.updateManagerNum();
-            try
+        new Thread(() -> {
+            for (; ; )
             {
-                Thread.sleep(1000);
+                StudentDAO.updateStudentNum();
+                ManagerDAO.updateManagerNum();
+                try
+                {
+                    Thread.sleep(1000);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
             }
-            catch (InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-        }
+        }).start();
     }
 }
